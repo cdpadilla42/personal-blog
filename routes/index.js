fc4952e494e14daceb0f60626faa7b198cad8352
Chris Padilla <cdpadilla42@gmail.com>
2022-05-18 14:04:45 -0500

    search for markdown file in folder

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,43 @@
 var express = require('express');
 var router = express.Router();
-const postsRouter = require('./posts');
-const usersRouter = require('./users');
 const showdown = require('showdown');
 const fs = require('fs');
 
 /* GET home page. */
-router.get('/:post', function (req, res, next) {
+router.get('/:post', function (req, res) {
   var postName = req.params.post;
-  // TODO: Garden. Pull weeds
 
-  // http://localhost:3000/TheSecretOfMonkeyIsland
-  fs.readFile(
-    `_posts/${postName}/TheSecretofMonkeyIsland.md`,
-    'utf8',
-    function (err, markdown) {
-      if (err) {
-        console.error(err);
-        // return 404
-        return;
+  const path = `_posts/${postName}`;
+  try {
+    const files = fs.readdirSync(path);
+    const fileName = files.find((file) => {
+      return file.includes('.md');
+    });
+
+    console.log(fileName);
+
+    fs.readFile(
+      `_posts/${postName}/${fileName}`,
+      'utf8',
+      function (err, markdown) {
+        if (err) {
+          console.error(err);
+          console.log(e);
+          res.sendStatus(404);
+          return;
+        }
+        const converter = new showdown.Converter();
+        const html = converter.makeHtml(markdown);
+        res.render('index', {
+          title: 'HOMEPAGE BABY',
+          body: html,
+        });
       }
-      const converter = new showdown.Converter();
-      const html = converter.makeHtml(markdown);
-      res.render('index', {
-        title: 'HOMEPAGE BABY',
-        body: html,
-      });
-    }
-  );
+    );
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(404);
+  }
 });
-
-// router.use('/posts', postsRouter);
-// router.use('/users', usersRouter);
 
 module.exports = router;
